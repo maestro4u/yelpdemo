f0fd2bd3224a5f2813ee9846fe922e506f01063e
maestro4u <huni.kim@gmail.com>
2015-07-04 20:44:35 +0900

    Set up links and routing for Reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -32,7 +32,7 @@ class ReviewsController < ApplicationController
 
     respond_to do |format|
       if @review.save
-        format.html { redirect_to root_path, notice: 'Review was successfully created.' }
+        format.html { redirect_to @restaurant, notice: 'Review was successfully created.' }
         format.json { render :show, status: :created, location: @review }
       else
         format.html { render :new }
@@ -74,7 +74,7 @@ class ReviewsController < ApplicationController
     def set_restaurant
       @restaurant = Restaurant.find(params[:restaurant_id])
     end
-    
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def review_params
       params.require(:review).permit(:rating, :comment)
